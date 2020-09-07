a5050ac68964709f2bd2a48bcb5d59d91fe9639b
Nicholas Merolle <nmerolle11@hotmail.com>
2020-09-07 20:05:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,15 +2,15 @@
 def create_array(n)
   array = []
   if n > 0
-    counter = 0
+    counter = 1
     while counter < n - 1
-      array << n
+      array << counter
       counter += 1
     end
   elsif n < 0
     counter = 0
     while counter > n + 1
-      array << n
+      array << counter
       counter -= 1
     end
     return array
@@ -20,5 +20,5 @@ def create_array(n)
     create_array(n)
     counter = 0
     while counter < array.size
-      if n % 
-  
+      array.each do |i|
+        
