28f4ab493b8fbff7faaa1f0f78b7e9b0e78659a6
Nicholas Merolle <nmerolle11@hotmail.com>
2020-09-07 20:07:13 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -7,12 +7,6 @@ def create_array(n)
       array << counter
       counter += 1
     end
-  elsif n < 0
-    counter = 0
-    while counter > n + 1
-      array << counter
-      counter -= 1
-    end
     return array
   end
   
