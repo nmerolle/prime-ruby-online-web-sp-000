56e64a320f754a0e60dfcd40ceb00a5f9a2e48f8
Nicholas Merolle <nmerolle11@hotmail.com>
2020-09-07 20:01:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,17 +3,22 @@ def create_array(n)
   array = []
   if n > 0
     counter = 0
-    while counter < n
+    while counter < n - 1
       array << n
       counter += 1
     end
   elsif n < 0
     counter = 0
-    while counter > n
+    while counter > n + 1
       array << n
       counter -= 1
     end
     return array
   end
   
+  def prime?(n)
+    create_array(n)
+    counter = 0
+    while counter < array.size
+      if array[counter] % 
   
