bee1c946b391aec40f0dc3dfec4014c2b3113183
Nicholas Merolle <nmerolle11@hotmail.com>
2020-09-07 20:21:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,24 +1,2 @@
 # Add  code here!
-def create_array(n)
-  array = []
-  if n > 0
-    counter = 2
-    while counter < n - 1
-      array << counter
-      counter += 1
-    end
-    return array
-  end
-  
-def prime?(n)
-  create_array(n)
-  array.each do |i|
-    if n % i == 0
-      return false
-    else 
-      return true 
-    end
-  end
-end
-end
 
